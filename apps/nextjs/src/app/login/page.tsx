ba5d3886aefbcd8a25555fe993418c33b8264265
LordPachimari <opachimari@gmail.com>
2023-11-25 10:45:30 +1100

    dynamic auth component

--- apps/nextjs/src/app/login/page.tsx
+++ apps/nextjs/src/app/login/page.tsx
@@ -1,5 +1,8 @@
-import HankoAuth from "~/components/other/auth/Hanko";
+import dynamic from "next/dynamic";
 
+const HankoAuth = dynamic(() => import("~/components/other/auth/Hanko"), {
+  ssr: false,
+});
 export default function LoginPage() {
   return (
     <div className="flex min-h-screen w-content flex-col items-center justify-center ">
